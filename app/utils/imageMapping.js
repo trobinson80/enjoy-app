ad392cec82f35c09828f63cea66097fa434feb34
trobinson80 <tgrobinson42@gmail.com>
2025-02-06 14:36:58 -0500

    added file to store the data and can cycle images

---
+++ app/utils/imageMapping.js
@@ -0,0 +1,10 @@
+// imageMapping.js
+export const imageMapping = {
+    //also update data/movies.json file
+    "step-brothers.jpg": require("../assets/step-brothers.jpg"),
+    "the-hangover.jpg": require("../assets/the-hangover.jpg"),
+    "the-internship.jpg": require("../assets/the-internship.jpg"),
+    "grown-ups.jpg": require("../assets/grown-ups.jpg"),
+    "wedding-crashers.jpg": require("../assets/wedding-crashers.jpg"),
+    "my-cousin-vinny.jpg": require("../assets/my-cousin-vinny.jpg")
+};
